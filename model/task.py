bc737a9fdae0612002b5c770c459090363ef40a4
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-10-01 22:16:30 -0300

    Feat: Novo método para mudar status e getters e setters
    Mudança na classe Task (Tarefas) implementa métodos necessários para acesso e alteração segura das informações da tarefa e método para mudar o status da tarefa (Completo/Não Completo)

--- model/task.py
+++ model/task.py
@@ -1,11 +1,11 @@
 class Task:
     """"""
     # Atributos
-    def __init__(self, __id, title, description, status) -> None:
+    def __init__(self, __id, title, description) -> None:
         self.__id = __id
         self.title = title
         self.description = description
-        self.status = status
+        self.status = False
         
     def get_task_info(self):
         
@@ -13,4 +13,26 @@ class Task:
         print(f"Task title: {self.title}")
         print(f"Task description: {self.description}")
         print(f"Task status: {self.status}")
+    
+    def change_status(self):
         
+        self.status = not self.status
+        
+    def get_id(self) -> int:
+        return self.__id
+    
+    def get_title(self) -> str:
+        return self.title
+
+    def set_title(self, new_title):
+        self.title = new_title
+        
+    def get_description(self) -> str:
+        return self.description
+    
+    def set_description(self, new_desc):
+        self.description = new_desc
+    
+    def get_status(self) -> bool:
+        return self.status
+    
